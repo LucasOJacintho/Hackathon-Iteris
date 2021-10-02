d022996806d7d51fbf6c69f60956aee64d1b13ec
Rafael Kikuchi <rafael.kikuchi1991@gmail.com>
2021-10-02 13:01:22 -0300

    Alteração

--- hackathon-iteris/src/views/Home.vue
+++ hackathon-iteris/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <SetembroAmarelo/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SetembroAmarelo from '../views/SetembroAmarelo.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SetembroAmarelo
   }
 }
 </script>
--- hackathon-iteris/src/views/SetembroAmarelo.vue
+++ hackathon-iteris/src/views/SetembroAmarelo.vue
@@ -1,17 +1,33 @@
 <template>
-    <v-container>
+    <v-container class="setembro-amarelo">
+        <h2>Setembro Amarelo</h2>
+        <hr/>
+        <p class="mt-2">
+            Pensamos nosso app para lhe fornecer algumas ideias de como se divertir praticando o isolamento social, mas se ainda assim
+            você achar que não está bem, não deixe de <strong>pedir ajuda!</strong>
+        </p>
     </v-container>
+
+
 </template>
 
 <script>
 export default {
-    name: 'SetembroAmarelo'
+    name: 'SetembroAmarelo',
     data() {
 
         return {
             referenciasSA: []
         }
         
+    },
+
+    created() {
+        fetch('https://it3zxc-default-rtdb.firebaseio.com/setembroamarelo.json')
+            .then(response => response.json)
+            .then(json => {
+                this.referenciaSA = json;
+            })
     }
 }
 </script>
