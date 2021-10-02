1342b08873530aaa6360cd35fdda6e0a503f8504
Claudio Cesar Guariento <claudiocguariento@hotmail.com>
2021-10-02 15:13:12 -0300

    mudanca

--- hackathon-iteris/src/views/jogos.vue
+++ hackathon-iteris/src/views/jogos.vue
@@ -2,11 +2,44 @@
     <v-container> 
         <h2 class= "text-h5 text-center mb-3 mt-5">Jogos</h2>
         <p> O Videogame ou game é uma disputa competitiva, executada no formato de um jogo eletrônico. O jogador permanece diante de um veículo que transmite as imagens do confronto lúdico, quase sempre uma TV ou o dispositivo de um computador. A seguir alguns exemplos: </p>
-        <ul>
-            <li>
-
-            </li>
-        </ul>
+        <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th colspan="2" class="text">
+            Genero
+          </th>
+          <th class="text">
+            Nome
+          </th>
+          <th class="text">
+            Foto
+          </th>
+          <th class="text">
+            Plataforma
+          </th>
+          <th class="text">
+            Ano
+          </th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(jogo) of jogos" :key="jogo.id">
+          <td>
+              <v-avatar size="24">
+                <img
+                    :src="jogo.photo"
+                    :alt="jogo.nome"
+                >
+                </v-avatar>
+              <span class="pl-2">{{ clube.nome }}</span>
+              </td>
+          <td class="text-right">{{ clube.pontos }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
     </v-container>
     
 </template>
@@ -16,15 +49,15 @@ export default {
     name: 'jogos',
     data () {
         return {
-            jogos: []
+            jogo: []
         }
     },
     created () {
         fetch ('https://it3zxc-default-rtdb.firebaseio.com/lazer/jogos.json')
         .then(resposta => resposta.json())
         .then(json => {
-            this.jogos = json;
-            console.log (this.jogos);
+            this.jogo = json;
+            console.log (this.jogo);
         });
     }
 }
