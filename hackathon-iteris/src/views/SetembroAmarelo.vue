34aeb1265d9b6345a63dbf4dd59467b4d98d6985
Rafael Kikuchi <rafael.kikuchi1991@gmail.com>
2021-10-02 12:17:31 -0300

    instalação do npm

---
+++ hackathon-iteris/src/views/SetembroAmarelo.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'SetembroAmarelo'
+    data() {
+
+        return {
+            referenciasSA: []
+        }
+        
+    }
+}
+</script>
+
+<style scoped>
+</style>
